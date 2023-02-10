ede377c3d677984cdd0cfcb5ba7578f543313edf
aesweb <aes.sncr@gmail.com>
2023-02-10 18:45:01 +0300

    build the search UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 const App = () => {
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-      Hello world
+      <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
+        <h1 className="text-4xl font-thin">
+          Weather <span className="font-black">Forecast</span>
+        </h1>
+        <p className="text-sm mt-2">
+          Enter below a place you want to know the weather of and select an
+          option from the dropdown
+        </p>
+        <div className="flex mt-10 md:mt-4">
+          <input
+            type="text"
+            value={''}
+            className="px-2 py-1 rounded-1-md border-2 border-white"
+          ></input>
+          <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-1 px-2 py-1 cursor-pointer">
+            search
+          </button>
+        </div>
+      </section>
     </main>
   )
 }
